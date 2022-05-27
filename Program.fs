2d2e69897d1dcd96c72fed5c19e91d641a358065
Chris Cheetham <chris@cheetham.com>
2022-05-27 11:03:21 -0400

    Update to net6.0
    Closes #4

--- Program.fs
+++ Program.fs
@@ -1,35 +1,36 @@
 namespace Sample
 
+#nowarn "20"
+
 open System
+open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 
 module Program =
     let exitCode = 0
 
-    let port =
-        if Environment.GetEnvironmentVariable("PORT") > ""
-            then Environment.GetEnvironmentVariable("PORT")
-            else "5000"
-    let url = String.Concat("http://0.0.0.0:", port)
-
-    let CreateHostBuilder args =
-        let mutable port =
-            Environment.GetEnvironmentVariable("PORT")
-
-        if port = null then port <- "8080"
-
-        let url =
-            String.Concat("http://0.0.0.0:", port)
-
-        Host
-            .CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(fun webBuilder ->
-                webBuilder.UseStartup<Startup>().UseUrls(url)
-                |> ignore)
-
     [<EntryPoint>]
     let main args =
-        CreateHostBuilder(args).Build().Run()
+
+        let builder = WebApplication.CreateBuilder(args)
+
+        let port =
+            if Environment.GetEnvironmentVariable("PORT") > "" then
+                Environment.GetEnvironmentVariable("PORT")
+            else
+                "8080"
+        let url = String.Concat("http://0.0.0.0:", port)
+        builder.WebHost.UseUrls(url)
+
+        builder.Services.AddControllers()
+
+        let app = builder.Build()
+
+        app.UseAuthorization()
+        app.MapControllers()
+
+        app.Run()
 
         exitCode
